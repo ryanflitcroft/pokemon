75b8220215588326ac3080e302d6cba2cb2a0892
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-02-08 19:51:03 -0800

    Function written for getPokemon to fetch data from client. Button elements added to PokemonPage to setPage onClick.

--- src/Pokemon/PokemonPage.js
+++ src/Pokemon/PokemonPage.js
@@ -1,13 +1,36 @@
 import React from 'react';
-import { useEffect, 
-  useState } from 'react';
+import { useEffect, useState } from 'react';
+import { getPokemon } from '../services/fetch-utils';
 
 
 
 export default function PokemonPage() {
+
+  const [pokemon, setPokemon] = useState([]);
+  const [page, setPage] = useState(1);
+  const PER_PAGE = 20;
+
+  useEffect(() => {
+    async function getData() {
+      const start = page * PER_PAGE - PER_PAGE + 1;
+      const end = page * PER_PAGE;
+      // console.log('||start', start, 'end', end);
+      const data = await getPokemon(start, end);
+      console.log('||data', data);
+      setPokemon(data);
+
+    }
+    getData();
+    console.log('||pokemon', pokemon);
+  }, [page]);
+
   return (
     <>
-    
+      <span>{page}</span>
+      <button onClick={() => setPage(page - 1)}
+        disabled={(page === 1)}>Previous</button>
+      <button onClick={() => setPage(page + 1)}
+        disabled={(pokemon.length < PER_PAGE)}>Next</button>
     </>
   );
 }
---
+++ src/services/fetch-utils.js
@@ -0,0 +1,12 @@
+import { client, 
+  checkError } from './client';
+
+export async function getPokemon(start, end) {
+  const response = await client
+    .from('pokemon')
+    .select()
+    .range(start, end);
+  console.log('||response', response.data);
+
+  return checkError(response);
+}
